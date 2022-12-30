4d8730a35487224362afb6d4c9129f32eb1d5ab1
CSFelix <csfelix08@gmail.com>
2022-12-29 23:42:26 -0300

    Deleted Kaggle Icon

--- src/containers/Feed/components/Service.tsx
+++ src/containers/Feed/components/Service.tsx
@@ -1,7 +1,6 @@
 import CONFIG from 'morethan-log.config'
 import React from 'react'
 import { AiOutlineBarChart, AiOutlineDeploymentUnit, AiOutlineFilePdf } from 'react-icons/ai'
-import KaggleIcon from '../../../assets/kaggle.svg';
 
 const Product: React.FC = () => {
   if (!CONFIG.projects) return null
@@ -33,8 +32,7 @@ const Product: React.FC = () => {
           target="_blank"
           className="p-3 hover:bg-gray-100 dark:hover:bg-zinc-700 rounded-2xl cursor-pointer flex items-center gap-3 text-gray-500 dark:text-white hover:text-black dark:hover:text-white "
         >
-          {/* <AiOutlineFilePdf className="text-2xl" /> */}
-          <KaggleIcon />
+          <AiOutlineFilePdf className="text-2xl" />
           <div className="text-sm">{CONFIG.projects[2].name}</div>
         </a>
       </ul>
